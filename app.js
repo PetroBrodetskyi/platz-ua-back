0fa6b0a08fb1a7e86faab265b211c3032930f245
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-07-04 23:41:28 +0200

    app updated

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use(express.json());
 app.use('/api/announcements', productsRouter);
 app.use("/api/users", usersRouter);
 
+app.use("/verify-email", (req, res, next) => {
+  req.url = `/api/users/verify-email${req.url}`;
+  next();
+});
+
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
