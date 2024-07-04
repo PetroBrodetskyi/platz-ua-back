39723f04d8a0bdb99d573c21b3af985c515cac03
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-07-04 17:21:14 +0200

    versel deploy

--- server.js
+++ server.js
@@ -14,27 +14,3 @@ mongoose.connect(DB_HOST, { dbName: 'platzbase' })
         console.log("Database connection error:", error.message);
         process.exit(1);
     });
-
-
-
-
-// import mongoose from "mongoose";
-// import app from "./app.js"
-
-// mongoose.Promise = global.Promise;
-
-// const { DB_HOST } = process.env;
-// const { PORT } = process.env;
-
-// mongoose.connect(DB_HOST)
-//     .then(() => {
-//         app.listen(PORT);
-//         console.log(`Server started on port: ${PORT}`);
-//         console.log("Database connection successful");
-//     })
-//     .catch(error => {
-//         console.log("Database connection error:", error.message);
-//         process.exit(1);
-//     });
-
-    
