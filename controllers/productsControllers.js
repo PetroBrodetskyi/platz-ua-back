5809fc031ee8c544aab0b99935cd668bffb27a48
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-07-30 12:44:59 +0200

    createProduct updated

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -90,6 +90,13 @@ export const createProduct = ctrlWrapper(async (req, res) => {
   const { name, price, description, condition, PLZ, city, category, subcategory1, subcategory2, subcategory3, delivery } = req.body;
   const owner = req.user._id;
 
+  let deliveryMethods;
+  try {
+    deliveryMethods = Array.isArray(delivery) ? delivery : JSON.parse(delivery || '[]');
+  } catch (error) {
+    return res.status(400).json({ message: '"delivery" must be an array' });
+  }
+  
   const uploadedUrls = req.files.map(file => file.path);
 
   const newProduct = {
@@ -107,7 +114,7 @@ export const createProduct = ctrlWrapper(async (req, res) => {
     subcategory1,
     subcategory2,
     subcategory3,
-    delivery,
+    delivery: deliveryMethods,
     owner,
   };
 
