9175e67d64c26532007676ca768b3bc6a1e45bca
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-08-09 17:51:42 +0200

    server updated

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 import app from "./app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const { DB_HOST, PORT } = process.env;
 
@@ -11,9 +13,35 @@ const mongooseOptions = {
   socketTimeoutMS: 45000
 };
 
+// Створюємо HTTP сервер з Express
+const server = createServer(app);
+
+// Ініціалізуємо socket.io
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Налаштуйте дозволені домени
+  },
+});
+
+// Обробка підключень через WebSocket
+io.on("connection", (socket) => {
+  console.log("Новий клієнт підключився", socket.id);
+
+  socket.on("message", (data) => {
+    console.log("Повідомлення від клієнта:", data);
+    
+    // Відправка повідомлення всім клієнтам
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Клієнт відключився", socket.id);
+  });
+});
+
 mongoose.connect(DB_HOST, mongooseOptions)
   .then(() => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server started on port: ${PORT}`);
       console.log("Database connection successful");
     });
