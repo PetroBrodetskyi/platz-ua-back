122649f9a1bfa3ac101b4317d7ad6761ad9fb9c3
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-10-07 17:11:00 +0200

    Styles change

--- models/userModel.js
+++ models/userModel.js
@@ -2,58 +2,61 @@ import mongoose from "mongoose";
 
 const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Name is required'],
+      type: String,
+      required: [true, "Name is required"],
     },
     phone: {
-        type: String,
-        required: [true, 'Phone number is required'],
+      type: String,
+      required: [true, "Phone number is required"],
     },
     password: {
-        type: String,
-        required: [true, 'Password is required'],
-        minlength: 6,
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
     },
     email: {
-        type: String,
-        match: [emailRegexp, 'Invalid email format'],
-        required: [true, 'Email is required'],
-        unique: true,
+      type: String,
+      match: [emailRegexp, "Invalid email format"],
+      required: [true, "Email is required"],
+      unique: true,
     },
     subscription: {
-        type: String,
-        enum: ["starter", "pro", "business", "admin"],
-        default: "starter"
+      type: String,
+      enum: ["starter", "pro", "business", "admin"],
+      default: "starter",
     },
     token: {
-        type: String,
-        default: null,
+      type: String,
+      default: null,
     },
     avatarURL: {
-        type: String,
-        default: null,
+      type: String,
+      default: null,
     },
     avatarPublicId: {
-        type: String,
-        default: null,
+      type: String,
+      default: null,
     },
     verify: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     verificationToken: {
-        type: String,
-        required: [true, 'Verify token is required'],
+      type: String,
+      required: [true, "Verify token is required"],
     },
-    likedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    likedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     likes: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
-}, { versionKey: false, timestamps: { createdAt: true } });
+  },
+  { versionKey: false, timestamps: { createdAt: true } }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,25 +1,26 @@
 import Joi from "joi";
 
 export const registerUserSchema = Joi.object({
-    name: Joi.string().required(),
-    phone: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+  name: Joi.string().required(),
+  phone: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
 });
 
 export const emailSchema = Joi.object({
-    email: Joi.string().email().required(),
-})
+  email: Joi.string().email().required(),
+});
 
 export const loginUserSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
 });
 
 export const updateUserSchema = Joi.object({
-    name: Joi.string().optional(),
-    phone: Joi.string().optional(),
-    email: Joi.string().email().optional(),
-    avatar: Joi.any().optional(),
-    likes: Joi.number().integer().min(0).optional()
+  name: Joi.string().optional(),
+  phone: Joi.string().optional(),
+  email: Joi.string().email().optional(),
+  avatar: Joi.any().optional(),
+  likes: Joi.number().integer().min(0).optional(),
+  cart: Joi.array().items(Joi.string()).optional(),
 });
