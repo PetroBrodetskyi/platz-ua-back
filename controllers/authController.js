79bde6671b1a20b1e439a156c8fb92906b5a3653
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-10-18 13:50:53 +0200

    authController updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,26 @@
 import * as userServices from "../services/usersServices.js";
 import HttpError from "../helpers/HttpError.js";
 import ctrlWrapper from "../helpers/ctrlWrapper.js";
-import { googleAuthSchema } from "../schemas/usersSchemas.js";
 import jwt from "jsonwebtoken";
+import { OAuth2Client } from "google-auth-library";
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+  const payload = ticket.getPayload();
+  return payload;
+}
 
 export const googleAuth = ctrlWrapper(async (req, res) => {
-  const { error } = googleAuthSchema.validate(req.body);
-  console.log("Received data:", req.body);
-  if (error) {
-    throw HttpError(400, error.message);
-  }
+  const { token } = req.body;
 
-  const { name, email, avatarURL } = req.body;
+  const payload = await verify(token);
+
+  const { name, email, picture } = payload;
 
   let user = await userServices.findUser({ email });
 
@@ -19,18 +28,18 @@ export const googleAuth = ctrlWrapper(async (req, res) => {
     user = await userServices.signup({
       name,
       email,
-      avatarURL,
+      avatarURL: picture,
     });
   }
 
-  const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+  const jwtToken = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
     expiresIn: "23h",
   });
 
-  await userServices.updateUserById(user._id, { token });
+  await userServices.updateUserById(user._id, { token: jwtToken });
 
   res.status(200).json({
-    token,
+    token: jwtToken,
     user: {
       name: user.name,
       email: user.email,
