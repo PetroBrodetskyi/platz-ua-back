b7708fb24750cc9c7fcfbab61e2aeb426c172f1e
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-11-05 12:38:54 +0100

    chatControllers updated

--- chat-server/routes/chatRoutes.js
+++ chat-server/routes/chatRoutes.js
@@ -6,13 +6,14 @@ import {
   getChatById,
   createChat,
 } from "../controllers/chatControllers.js";
+import { authenticate } from "../../middlewares/authenticate.js";
 
 const router = express.Router();
 
-router.get("/messages", getMessages);
+router.get("/messages", authenticate, getMessages);
 router.post("/messages", sendMessage);
 router.get("/chats", getChats);
-router.get("/chats/:chatId", getChatById);
+router.get("/chats/:chatId", authenticate, getChatById);
 router.post("/chats", createChat);
 
 export default router;
